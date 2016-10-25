e8decf51c81ef2f501e840b7636514dbeec24002
Vladislav Belous <oksiwhite@ukr.net>
2016-10-25 16:34:33 +0300

    Initial commit

---
+++ app/src/main/java/com/devqt/kubg/informer/Il_Fragments.java
@@ -0,0 +1,14 @@
+package com.devqt.kubg.informer;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Il_Fragments extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.il_fragment);
+    }
+}
