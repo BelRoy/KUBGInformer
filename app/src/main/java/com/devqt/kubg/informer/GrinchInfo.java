3a3c2d848d1e911861e5af4536a515b92943933f
Vladislav Belous <oksiwhite@ukr.net>
2016-10-25 17:26:32 +0300

    Package fragment + parse title site in listview columns

--- app/src/main/java/com/devqt/kubg/informer/GrinchInfo.java
+++ app/src/main/java/com/devqt/kubg/informer/GrinchInfo.java
@@ -1,7 +1,10 @@
 package com.devqt.kubg.informer;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -9,9 +12,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.devqt.kubg.informer.fragments.Il_Fragments;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class GrinchInfo extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    public Elements content;
+    public ArrayList<String> titleList = new ArrayList<>();
+    private ArrayAdapter<String> adapter;
+    private ListView listView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +50,11 @@ public class GrinchInfo extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        listView = (ListView) findViewById(R.id.list);
+        new NewThread().execute();
+        adapter = new ArrayAdapter<>(this, R.layout.listview_row, R.id.pro_item, titleList);
+
     }
 
     @Override
@@ -67,9 +93,14 @@ public class GrinchInfo extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
 
+        Fragment fragment = null;
+        Class fragmentClass = null;
+
         int id = item.getItemId();
 
         if (id == R.id.il) {
+
+            fragmentClass = Il_Fragments.class;
 
         } else if (id == R.id.pi) {
 
@@ -91,8 +122,43 @@ public class GrinchInfo extends AppCompatActivity
 
         }
 
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.nav_view, fragment).commit();
+
+        item.setChecked(true);
+
+        setTitle(item.getTitle());
+
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public class NewThread extends AsyncTask<String, Void, String> {
+
+
+        @Override
+        protected String doInBackground(String... arg) {
+            Document document;
+            try {
+                document = Jsoup.connect("http://kubg.edu.ua/prouniversitet/news/studentam.html").get();
+                content = document.select("contentheading");
+                titleList.clear();
+                for (Element contents : content) {
+                    titleList.add(contents.text());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
     }
 }
